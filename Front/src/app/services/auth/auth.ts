dde5f0896ca7379c6fe1333ecb9b254707a81420
gonalvarado <gonalvarado86@gmail.com>
2024-05-27 12:05:38 -0300

    Corrigiendo errores para que compile la aplicación

--- Front/src/app/services/auth/auth.ts
+++ Front/src/app/services/auth/auth.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export interface UserCredentials {
     username: string,
     password: string,
@@ -23,80 +21,4 @@ export interface Update {
   telefono: string;
   idpu: number
   id: number,
-=======
-export interface UserCredentials {
-    username: string,
-    password: string,
-  }
- 
-  export interface LoggedInUser {
-    id: number,
-    //token: string,
-    username: string
-    pacienteUser: number
-    idpu: number
-  }
-
-export interface Update {
-  apellido: string;
-  clave: string;
-  dni_paciente: string;
-  email: string;
-  foto: string;
-  nombre: string;
-  telefono: string;
-  idpu: number
-  id: number,
->>>>>>> desarrolladorJEOrellano
-=======
-<<<<<<< HEAD
-export interface UserCredentials {
-    username: string,
-    password: string,
-  }
- 
-  export interface LoggedInUser {
-    id: number,
-    //token: string,
-    username: string
-    pacienteUser: number
-    idpu: number
-  }
-
-export interface Update {
-  apellido: string;
-  clave: string;
-  dni_paciente: string;
-  email: string;
-  foto: string;
-  nombre: string;
-  telefono: string;
-  idpu: number
-  id: number,
-=======
-export interface UserCredentials {
-    username: string,
-    password: string,
-  }
- 
-  export interface LoggedInUser {
-    id: number,
-    //token: string,
-    username: string
-    pacienteUser: number
-    idpu: number
-  }
-
-export interface Update {
-  apellido: string;
-  clave: string;
-  dni_paciente: string;
-  email: string;
-  foto: string;
-  nombre: string;
-  telefono: string;
-  idpu: number
-  id: number,
->>>>>>> a95aded2ac569ad11b01f9f9be9898951600c962
->>>>>>> isaiasruben
 }
