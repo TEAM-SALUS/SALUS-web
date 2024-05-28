47cbf42d33d877c371fcedf36719ee8e306e82af
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-05-28 00:56:30 -0300

    actualizando repo local

--- Back/venv/Scripts/activate.bat
+++ Back/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\Desktop\Salus_b\SALUS-web\Back\venv
+set VIRTUAL_ENV=C:\Users\Esteban\Desktop\Escritorio\Git\SALUS-web\Back\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
